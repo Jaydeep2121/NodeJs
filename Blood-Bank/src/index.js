c68061131d335b8f98996183b51f6c8451a53822
Jaydeep <jaydipc95@gmail.com>
2022-04-25 18:35:58 +0530

    save

--- Blood-Bank/src/index.js
+++ Blood-Bank/src/index.js
@@ -4,7 +4,11 @@ require("./db/mongoose");
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(cors());
+app.use(cors({
+  credentials:true,
+  origin:['http://localhost:4200']
+}));
+app.use(require('cookie-parser')());
 app.use(express.json());
 app.use(require('./middleware/middlewares'));
 app.listen(port, () => {
--- Blood-Bank/src/routers/admin_Router.js
+++ Blood-Bank/src/routers/admin_Router.js
@@ -1,4 +1,5 @@
 const express = require("express");
+var jwt = require('jsonwebtoken');
 const Myadmin = require("../models/admin_Model");
 const router = new express.Router();
 
@@ -18,10 +19,42 @@ router.post('/admin/login',async (req,res)=>{
     try {
         const admin = await Myadmin.findByCredentials(req.body.email,req.body.password);
         const token = await admin.generateAuthToken();
-        res.status(201).send({token});
+        res.cookie('jwt',token,{
+            httpOnly:false, // for more secure and can't acces from outside if true
+            nextAge:24*60*60*1000 //1 day
+        })
+        res.send({
+            message:'success'
+        });
     } catch (error) {
         console.log('e',error);
         res.status(400).send(error);
     }
+})
+//authenticate admin
+router.get('/auth-admin',async (req,res)=>{
+    try {
+        const cookie = req.cookies['jwt'];
+        const claims = jwt.verify(cookie,'thisisdemo');
+        if(!claims){
+            return res.status(401).send({
+                message:'unauthenticate'
+            })
+        }
+        const admin = await Myadmin.findOne({_id:claims._id});
+        const {password,...data} = await admin.toJSON();
+        res.send(data);   
+    } catch (error) {
+        return res.status(401).send({
+            message:'unauthenticate'
+        })
+    }
+})
+//logout 
+router.post('/logout',async (req,res)=>{
+    res.cookie('jwt',null,{maxAge:0})
+    res.send({
+        message:'logout success'
+    })
 })
 module.exports=router;
