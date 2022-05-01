a946b134663a6beb84f1b6006a4e128922761bac
Jaydeep <jaydipc95@gmail.com>
2022-05-01 11:54:25 -0500

    save

--- Blood-Bank/src/routers/user_Router.js
+++ Blood-Bank/src/routers/user_Router.js
@@ -1,49 +1,61 @@
 const express = require('express');
 const MyUser = require('../models/user_Model');
-const multer  = require('multer');
+const multer = require('multer');
 const router = new express.Router();
 
 var FileStorage = multer.diskStorage({
-    destination: (req, file, cb) => {
-      cb(null, '../../NodeJS/Blood-Bank/public/images');
-    },
-    filename: (req, file, cb) => {
-      console.log(file);
-      var filetype = '';
-      if(file.mimetype === 'image/jpeg') {
-        filetype = 'jpeg';
-      }
-      if(file.mimetype === 'image/png') {
-        filetype = 'png';
-      }
-      if(file.mimetype === 'image/jpeg') {
-        filetype = 'jpg';
-      }
-      cb(null, 'image-' + Date.now() + '.' + filetype);
+  destination: (req, file, cb) => {
+    cb(null, '../../NodeJS/Blood-Bank/public/images');
+  },
+  filename: (req, file, cb) => {
+    console.log(file);
+    var filetype = '';
+    if (file.mimetype === 'image/jpeg') {
+      filetype = 'jpeg';
     }
+    if (file.mimetype === 'image/png') {
+      filetype = 'png';
+    }
+    if (file.mimetype === 'image/jpeg') {
+      filetype = 'jpg';
+    }
+    cb(null, 'image-' + Date.now() + '.' + filetype);
+  }
 });
-var upload = multer({storage:FileStorage});
+var upload = multer({ storage: FileStorage });
 
 //for save data
-router.post('/users',upload.single('file'),async (req,res)=>{
-    if(!req.file) {
-        return res.status(500).send({ message: 'Upload fail'});
-    } else {
-        req.body.imageUrl = '../../public/images' + req.file.filename;
-        MyUser.create(req.body, function (err, gallery) {
-            if (err) {
-                console.log('route error:',err);
-                return next(err);
-            }
-            res.json(gallery);
-        });
-    }
+router.post('/users', upload.single('file'), async (req, res) => {
+  if (!req.file) {
+    return res.status(500).send({ message: 'Upload fail' });
+  } else {
+    req.body.imageUrl = '../../public/images' + req.file.filename;
+    MyUser.create(req.body, function (err, gallery) {
+      if (err) {
+        console.log('route error:', err);
+        return next(err);
+      }
+      res.json(gallery);
+    });
+  }
 })
 //get data by id
-router.get('/:id',async (req,res)=>{
-  MyUser.findById(req.params.id,(err,gallery)=>{
-    if(err)return next(err);
+/*
+router.get('/:id', async (req, res) => {
+  MyUser.findById(req.params.id, (err, gallery) => {
+    if (err) return next(err);
     res.json(gallery);
   })
 })
-module.exports=router
+*/
+// To Get List Of Employees
+router.get('/getUsers', async (req, res) => {
+  try {
+    const user = await MyUser.find({});
+    res.send(user);
+  } catch (error) {
+    console.log('e', error);
+  }
+});
+
+module.exports = router
