6cf3ea7fadc0821d7fc29dfe8751ca8b13a2d45f
Jaydeep <jaydipc95@gmail.com>
2022-06-17 18:37:39 +0530

    save

--- Blood-Bank/src/db/mongoose.js
+++ Blood-Bank/src/db/mongoose.js
@@ -1,6 +1,22 @@
 const mongoose = require("mongoose");
-// mongoose.connect("mongodb+srv://jaydeep:abcdefg@cluster0.5wyzh.mongodb.net/?retryWrites=true&w=majority")
-mongoose.connect("mongodb://127.0.0.1:27017/blood-bank-api", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-});
+mongoose
+  .connect(
+    "mongodb+srv://jaydeep:abcdefg@cluster0.5wyzh.mongodb.net/blood_bank_db?retryWrites=true&w=majority",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  )
+  .then(() => {
+    console.log("connection success");
+  })
+  .catch((err) => {
+    console.log("no connection");
+  });
+
+/* Local Connection 
+    mongoose.connect("mongodb://127.0.0.1:27017/blood-bank-api", {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    });
+  */
